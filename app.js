4333b42194de661f344989e583be20c40c683500
Tomas Barina <tomasbarina@Tomass-MacBook-Pro.local>
2015-12-07 09:00:23 +0100

    Some cleaning

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var FormData = require('form-data');
 var multiparty = require('multiparty');
-var http = require('http');
-var util = require('util');
-
-
-
-var routes = require('./routes/index');
-var capture = require('./routes/capture');
-var http = require('http');
-var request = require('request');
 var fs = require('fs');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 //app.use('/capture', capture);
 
 app.get('/capture', function (req, res) {
     res.sendFile(path.join(__dirname + '/capture.html'));
 });
 
-app.get('capture')
+app.get('/list', function(req,res) {
+   res.sendFile(path.join(__dirname + 'list.html')); 
+});
 
 
 app.post('/recognize', function (req, res) {
@@ -84,32 +70,16 @@ app.post('/recognize', function (req, res) {
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
-    next(err);
+    res.write('Page not found');
+     res.end();
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+     res.write('error:'+err.message);
+     res.end();
+ });
 
 
 module.exports = app;
